1b3ccb6ed1ab2813c96a4ceff47d15542bab2a0d
David Gamero <david340804@gmail.com>
2020-08-25 18:03:23 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse';
+import { Line, ResponsiveLine } from '@nivo/line'
+import moment from 'moment';
 
 function App() {
+  const CSV_URL = 'https://gatech-covid-19-data.s3.amazonaws.com/gatech_covid_data.csv';
+
+  let [data, setData] = useState();
+
+  useEffect(() => {
+    Papa.parse(CSV_URL,
+      {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        complete: (results, file) => {
+          console.log(results);
+
+          // Filter for valid rows
+          data = results.data.filter((row) =>
+            row.date && row.cases);
+          data = data.reverse();
+          console.log(`Found ${data.length} valid rows from ${data[0].date} to ${data[data.length - 1].date}`)
+
+          // Map to xy
+          data = data.map((row) => ({
+            x: Date.parse(row.date),
+            y: row.cases
+          }));
+          console.log(data);
+          setData([{
+            id: 0,
+            data: data
+          }]);
+        }
+      })
+  }, []);
+
+  const theme = {
+    textColor: '#ffffff',
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          data ?
+            <Line
+              data={data}
+              isInteractive={true}
+              width={window.innerWidth * 0.8}
+              height={window.innerHeight * 0.8}
+              curve="monotoneX"
+              yFormat={(y) => '' + y}
+              xFormat={(x) =>
+                moment(x).format('MMM D, YYYY')
+              }
+              enableGridX={false}
+              enableGridY={false}
+              theme={theme}
+              margin={{
+                bottom: 50,
+                top: 50,
+                left: 50,
+                right: 50
+              }}
+            /> : 'Loading'
+        }
       </header>
     </div>
   );
