f8b03f074724ba6fc708c7e18270531b89e834cd
David Gamero <david340804@gmail.com>
2020-08-26 00:18:36 -0400

    Updates

---
+++ precache-manifest.c4d393daa8e7d159142d7c8067a8a733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed30e4fc2f3763b242ed618a8a11e888",
+    "url": "/gatech-covid-chart/index.html"
+  },
+  {
+    "revision": "b477f4a9391caae7143b",
+    "url": "/gatech-covid-chart/static/css/main.ce8dc3c8.chunk.css"
+  },
+  {
+    "revision": "c1ae5186652ad3f02128",
+    "url": "/gatech-covid-chart/static/js/2.1dde127c.chunk.js"
+  },
+  {
+    "revision": "74a676a9bb0872088eb7399c04d7f0b1",
+    "url": "/gatech-covid-chart/static/js/2.1dde127c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b477f4a9391caae7143b",
+    "url": "/gatech-covid-chart/static/js/main.ab499c8a.chunk.js"
+  },
+  {
+    "revision": "e66b96f16a3c5b36b21c",
+    "url": "/gatech-covid-chart/static/js/runtime-main.cf73bc0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gatech-covid-chart/static/media/logo.5d5d9eef.svg"
+  }
+]);
